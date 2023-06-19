57b6a31bdcedd2c03f5507917d1f60336a86e2ff
Anav Chand <anavkki@gmail.com>
2023-06-19 14:44:23 +1200

    e

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -1,18 +1,55 @@
-console.log("Firebase Auth 🔑")
-
+// Function imports for firebase auth and firebase firestore
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import { getFirestore, collection, getDocs,onSnapshot  } from "firebase/firestore";
 import { app } from "../firebase/client";
 
+// Initialise firebase firestore database
+const db = getFirestore(app);
+
+// Funtion to take data and inject into html template
+const populate = (snapshot) => {
+    let data = []
+    let html = ''
+    snapshot.docs.forEach( (doc) => {
+        data.push({ ...doc.data(), id: doc.id})
+        html += `
+        <div class="p-5 rounded-md shadow-xl col-span-1">
+        <h1 class="text-xl font-bold">${doc.data().Title}</h1>
+        <p>${doc.data().Content}</p>
+        </div>
+        `
+    })
+    const card = document.querySelector(".main")
+    card.innerHTML = html;
+    console.log("Firebase Firestore 🔥")
+}
+
+// Initialise firebase Auth 
 const auth = getAuth(app);
+
+// Check if user is logged in or not
 auth.onAuthStateChanged( (user) => {
     if(user) {
-        console.log("User Logged Iin", user)
+        console.log("Firebase Auth 🔑")
+        console.log("User Logged In", user)
+        
+        const colrRef = collection(db, "Tips")
+
+        onSnapshot(colrRef, (snapshot) => {
+            populate(snapshot)
+            console.log(snapshot.docs)
+        })
     }
     else {
-        console.log("User Logged Out")
+         document.querySelector(".main").innerHTML = `
+         <div class="p-5 text-center rounded-md shadow-xl col-span-3">
+         <h1 class="text-xl font-semibold">Log In to see data</h1>
+         </div>
+         `
     }
 })
 
+// Firebase auth signup
 const signupForm = document.querySelector("#signup")
 signupForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -32,24 +69,27 @@ signupForm.addEventListener("submit", (e) => {
     })
 })
 
-    const logOutBtn = document.querySelector("#logout");
-    
-logOutBtn.addEventListener("click",(e) => {
-    e.preventDefault();
-    auth.signOut()
-})
-
+// Firebase auth sigin
 const loginForm = document.querySelector("#login")
 loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const email = loginForm['login-firebase-email'].value;
     const password = loginForm['login-firebase-password'].value;
-
+    
     signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {            
-
+        
         loginForm.classList.remove("show")
         loginForm.reset();
-
+        
     })
+})
+
+// Firebase auth signout
+const logOutBtn = document.querySelector("#logout");    
+logOutBtn.addEventListener("click",(e) => {
+    console.log("Firebase Auth 🔑")
+    console.log("User Logged Out")
+    e.preventDefault();
+    auth.signOut()
 })
