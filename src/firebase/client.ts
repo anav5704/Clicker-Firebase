54aba2b23d86c5534232708d643dfc20a04e3e75
Anav Chand <anavkki@gmail.com>
2023-06-24 18:47:40 +1200

    e

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCUHjneWuraiglHWM3tyA8-5equI4R9cJ4",
@@ -7,9 +6,7 @@ const firebaseConfig = {
   projectId: "clicker-efa4f",
   storageBucket: "clicker-efa4f.appspot.com",
   messagingSenderId: "562627723723",
-  appId: "1:562627723723:web:56028e168e03b50d1a364e",
-  measurementId: "G-C3EFR4TRHG"
+  appId: "1:562627723723:web:56028e168e03b50d1a364e"
 };
 
 export  const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
