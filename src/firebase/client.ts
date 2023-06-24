98375c69fbd69e792cc286138f87ef7ae86d88ab
Anav Chand <anavkki@gmail.com>
2023-06-24 17:56:35 +1200

    r

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,6 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCUHjneWuraiglHWM3tyA8-5equI4R9cJ4",
   authDomain: "clicker-efa4f.firebaseapp.com",
@@ -8,8 +13,9 @@ const firebaseConfig = {
   storageBucket: "clicker-efa4f.appspot.com",
   messagingSenderId: "562627723723",
   appId: "1:562627723723:web:56028e168e03b50d1a364e",
-  measurementId: "G-VR5YDEZDWX"
+  measurementId: "G-7YW2YG7FRE"
 };
 
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
